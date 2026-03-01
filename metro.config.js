d249b4a347a82ec4c776781935a7a25818a2f48d
Arena Of Heroes <arenaofheroes2025@gmail.com>
2026-03-01 01:30:04 -0300

    Fix Expo web bundling on Vercel

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,10 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const isCi = process.env.CI === "true" || Boolean(process.env.VERCEL);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
+  // In CI/Vercel keep CSS virtual to avoid SHA-1 lookup issues on generated cache files
+  forceWriteFileSystem: !isCi,
 });
